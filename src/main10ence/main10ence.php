0dd00ed394fedbb6c8d1851f6c9d912a1873cf6c
10twebdesign <brock@10twebdesign.com>
2015-03-27 20:33:28 -0400

    Updating main10ence.php.

---
+++ src/main10ence/main10ence.php
@@ -0,0 +1,10 @@
+/*
+Plugin Name: Main10ence
+Plugin URI: http://www.10twebdesign.com/
+Description: Automatic maintenace plugin for Wordpress.
+Version: 0.1a
+Author: 10T Web Design
+Author URI: http://www.10twebdesign.com/
+GitHub Plugin URI: https://github.com/10twebdesign/main10ence
+License: GPL2
+*/
